982dd0d1e8bb79cbb6f348220bec5b91b2a80dac
Jordi Sabaté <jordi@webcoop.cat>
2020-04-11 17:20:37 +0200

    privacy policy

--- src/components/Privacy.vue
+++ src/components/Privacy.vue
@@ -5,9 +5,9 @@
 <h2 class="title">Polítiques de privacitat</h2>
 <b>1. Responsable del tractament</b>
 
-Responsable del tractament: Jordi Sabaté i Estopà (webcoop.cat), (en endavant Webcoop)
+Responsable del tractament: Jordi Sabaté i Estopà, (en endavant EDUCOV)
 
-Dades de contacte de Webcoop: Jordi Sabaté i Estopà. Carrer Bernat Saulet 25, 2-2, 17860 Sant Joan de les Abadesses. NIF 39891236J, hola@webcoop.cat.
+Dades de contacte d'EDUCOV: Jordi Sabaté i Estopà. Carrer Bernat Saulet 25, 2-2, 17860 Sant Joan de les Abadesses. NIF 39891236J, hola@webcoop.cat.
 
 Contacte del delegat de protecció de dades: hola@webcoop.cat
 
@@ -15,7 +15,7 @@ Contacte del delegat de protecció de dades: hola@webcoop.cat
 
 Les seves dades personals només s’utilitzaran per a les següents finalitats:
 
-a) Gestionar l’enviament d’informació dels actes, activitats i informacions de EDUCOV, per mitjans electrònics o correu convencional.
+a) Gestionar l’enviament d’informació dels actes, activitats i informacions d'EDUCOV, per mitjans electrònics o correu convencional.
 
 b) No tractarem les seves dades personals amb una altra finalitat que les aquí descrites excepte per obligació legal o requeriment judicial.
 
@@ -29,7 +29,7 @@ La base legal per al tractament és el consentiment de l’interessat.
 
 <b>5. Lloc on es guarda la informació</b>
 
-La informació proporcionada es guarda en els servidors de webcoop.cat o de proveïdors i empreses encarregats del tractament de dades que tinguin els servidors en el territori de la Unió Europea o estiguin emparats pel Privacy Shield.
+La informació proporcionada es guarda en els servidors de educov.cat o de proveïdors i empreses encarregats del tractament de dades que tinguin els servidors en el territori de la Unió Europea o estiguin emparats pel Privacy Shield.
 
 <b>6. Cessió i transferència</b>
 
@@ -37,7 +37,7 @@ Les dades facilitades no es cediran a organismes ni empreses externes.
 
 <b>7. Drets de les persones interessades  </b>
 
-Pot sol·licitar l’exercici dels següents drets davant webcoop presentant un escrit a l’adreça postal de la capçalera o enviant un missatge electrònic a hola@webcoop.cat i adjuntant una fotocòpia del DNI o qualsevol altre document anàleg en dret, com indica la llei.
+Pot sol·licitar l’exercici dels següents drets davant EDUCOV presentant un escrit a l’adreça postal de la capçalera o enviant un missatge electrònic a hola@webcoop.cat i adjuntant una fotocòpia del DNI o qualsevol altre document anàleg en dret, com indica la llei.
 
 <b>8. Drets</b>
 
@@ -63,7 +63,7 @@ L’interessat també té dret a presentar una reclamació davant l’autoritat 
 
 <b>9. Seguretat     </b>
 
-Webcoop, en resposta a la confiança dipositada en nosaltres i tenint en compte la importància en matèria de protecció i confidencialitat que requereixen les dades personals de l’usuari, l’informa que ha adoptat totes les mesures tècniques i organitzatives necessàries per salvaguardar la seva seguretat, tal com exigeix ​​la normativa vigent que regula el reglament de mitjans de seguretat dels fitxers que continguin dades de caràcter personal.
+EDUCOV, en resposta a la confiança dipositada en nosaltres i tenint en compte la importància en matèria de protecció i confidencialitat que requereixen les dades personals de l’usuari, l’informa que ha adoptat totes les mesures tècniques i organitzatives necessàries per salvaguardar la seva seguretat, tal com exigeix ​​la normativa vigent que regula el reglament de mitjans de seguretat dels fitxers que continguin dades de caràcter personal.
 
 <b>10. Galetes (cookies)   </b>
 
@@ -77,17 +77,17 @@ Les dades de navegació recuperats per les galetes són anònims i no estan asso
 
 L’usuari podrà esborrar o desactivar les galetes des de la configuració dels navegadors. En aquest cas, la pàgina web continuarà operativa, però sense els avantatges de la personalització. Per a més detalls sobre l’ús, la gestió i la configuració de les galetes dels navegadors, es recomana consultar el web http://www.aboutcookies.org .
 
-2. Quin tipus de galetes s’utilitzen a webcoop?     
+2. Quin tipus de galetes s’utilitzen a EDUCOV?     
 
 Segons l’entitat que les gestiona:           
 
-Pròpies. Són galetes que s’envien des d’un equip o domini gestionat per Webcoop al terminal de l’usuari.
+Pròpies. Són galetes que s’envien des d’un equip o domini gestionat per EDUCOV al terminal de l’usuari.
 
-De tercers. Són galetes que s’envien al terminal de l’usuari des d’un equip o domini no gestionat ni controlat per Webcoop. En aquest cas s’inclouen també les galetes instal·lades per Webcoop que recopilen informació gestionada per tercers. Les galetes de tercers poden utilitzar-se per al mesurament i l’anàlisi del comportament dels usuaris amb l’objectiu de millorar la seva experiència al web de Webcoop.                      
+De tercers. Són galetes que s’envien al terminal de l’usuari des d’un equip o domini no gestionat ni controlat per EDUCOV. En aquest cas s’inclouen també les galetes instal·lades per Webcoop que recopilen informació gestionada per tercers. Les galetes de tercers poden utilitzar-se per al mesurament i l’anàlisi del comportament dels usuaris amb l’objectiu de millorar la seva experiència al web de Webcoop.                      
 
 Segons el termini de temps que romandrà encesa:        
 
-De sessió. Recullen dades mentre l’usuari navega per la web de Webcoop amb la finalitat de prestar el servei sol·licitat. Caduquen o s’esborren quan es tanca el navegador.
+De sessió. Recullen dades mentre l’usuari navega per la web de EDUCOV amb la finalitat de prestar el servei sol·licitat. Caduquen o s’esborren quan es tanca el navegador.
 
 Persistents. Es guarden en el terminal de l’usuari. Caduquen en un període llarg o mitjà de temps o no ho fan mai.         
 
@@ -97,17 +97,17 @@ Tècniques. Són les necessàries per a la correcta navegació per la pàgina we
 
 De personalització. Permeten a l’usuari definir les característiques (idioma) per a la navegació per la pàgina web.
 
-D’anàlisi. Permeten a Webcoop l’anàlisi vinculat a la navegació realitzada per l’usuari per tal de dur a terme un seguiment de l’ús de la pàgina web, així com fer estadístiques dels continguts més visitats, del nombre de visitants, etc.        
+D’anàlisi. Permeten a EDUCOV l’anàlisi vinculat a la navegació realitzada per l’usuari per tal de dur a terme un seguiment de l’ús de la pàgina web, així com fer estadístiques dels continguts més visitats, del nombre de visitants, etc.        
 
 Publicitàries.
 
-3. Quines són les finalitats de les galetes utilitzades a Webcoop?  
+3. Quines són les finalitats de les galetes utilitzades a EDUCOV?  
 
-Les finalitats de les galetes utilitzades a Webcoop són:      
+Les finalitats de les galetes utilitzades a EDUCOV són:      
 
 Tècniques. L’objectiu d’aquest tipus de galetes és fer possible que la navegació per la pàgina sigui satisfactòria. Aquest tipus de galetes permet que el sistema reconegui als usuaris registrats i els recordi el nom d’usuari i la contrasenya, entre altres funcions.   
 
-De personalització. Aquestes galetes permeten que l’usuari accedeixi al web de Webcoop amb algunes característiques de caràcter general predefinides, com ara l’idioma, el navegador o la localització geogràfica des d’on s’accedeix al servei. Aquesta informació dels usuaris permet a Webcoop poder oferir un millor servei i més personalitzat.        
+De personalització. Aquestes galetes permeten que l’usuari accedeixi al web d'EDUCOV amb algunes característiques de caràcter general predefinides, com ara l’idioma, el navegador o la localització geogràfica des d’on s’accedeix al servei. Aquesta informació dels usuaris permet a Webcoop poder oferir un millor servei i més personalitzat.        
 
 Analítiques. Les dades anònims continguts en aquestes galetes permeten el mesurament, el seguiment i l’estudi de l’activitat dels usuaris per tal d’incorporar millores en els llocs web, aplicacions i plataformes de Webcoop.       
 
@@ -115,7 +115,7 @@ Publicitàries. Aquestes galetes fan possible la gestió dels espais publicitari
 
 <b>11. Canvis en la present política de privacitat  </b>
 
-Webcoop es reserva el dret a modificar la present política per adaptar-la a novetats legislatives o jurisprudencials.
+EDUCOV es reserva el dret a modificar la present política per adaptar-la a novetats legislatives o jurisprudencials.
 
     </section>
   </div>
